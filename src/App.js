ee51a0e6b73378649e9d448f63a98b561c3cec64
rforman89 <rforman89@gmail.com>
2026-06-07 11:46:01 +0200

    updating logo to JPG

--- src/App.js
+++ src/App.js
@@ -6629,7 +6629,7 @@ export default function App() {
               }}
             >
               <img
-                src="/CSI-HIT_4logo.svg"
+                src="/csi-hit-logo.jpg"
                 alt="CSI HIT Alphen logo"
                 style={{
                   width: "min(500px, 90vw)",
