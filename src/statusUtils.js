42ae5d4b3287ebfd5dae775e11e24203952e80b7
rforman89 <rforman89@gmail.com>
2026-06-17 22:04:20 +0200

    Delete src/statusUtils.js

--- src/statusUtils.js
+++
@@ -1,6 +0,0 @@
-export function getStatusLabel(status) {
-  if (status === "suspect") return "Verdacht";
-  if (status === "doubt") return "Twijfel";
-  if (status === "excluded") return "Uitgesloten";
-  return "Onbekend";
-}
