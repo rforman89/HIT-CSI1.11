0a89820e6c72270f4816f1928171adc9fee8969b
rforman89 <rforman89@gmail.com>
2026-07-30 08:36:46 +0200

    Update AdminGroupsList.jsx

--- src/components/admin/AdminGroupsList.jsx
+++ src/components/admin/AdminGroupsList.jsx
@@ -314,6 +314,7 @@ export default function AdminGroupsListPanel({ ctx }) {
     SuspectDashboard,
     LandingPage,
     LoginScreen,
+    openClueFile,
   } = ctx;
 
   const AdminPurchasesOverview = () => {
@@ -629,14 +630,21 @@ export default function AdminGroupsListPanel({ ctx }) {
 
                           {(clue?.file_url || clue?.pdf_url) && (
                             <div style={{ marginTop: 8 }}>
-                              <a
-                                href={clue.file_url || clue.pdf_url}
-                                target="_blank"
-                                rel="noreferrer"
-                                style={styles.link}
+                              <button
+                                type="button"
+                                onClick={() =>
+                                  openClueFile(clue.file_url || clue.pdf_url)
+                                }
+                                style={{
+                                  ...styles.link,
+                                  background: "none",
+                                  border: "none",
+                                  padding: 0,
+                                  cursor: "pointer",
+                                }}
                               >
                                 Bestand openen
-                              </a>
+                              </button>
                             </div>
                           )}
                           <div style={{ marginTop: 10 }}>
